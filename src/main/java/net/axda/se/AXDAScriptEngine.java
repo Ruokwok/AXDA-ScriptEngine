8f9eb47734391f8ecf643f70fab945f13f793618
Ruokwok <i@ruok.cc>
2025-07-21 17:37:49 +0800

    添加ll接口对象的属性

--- src/main/java/net/axda/se/AXDAScriptEngine.java
+++ src/main/java/net/axda/se/AXDAScriptEngine.java
@@ -7,9 +7,12 @@ import java.io.File;
 public class AXDAScriptEngine extends PluginBase {
 
     public static final File PLUGIN_PATH = new File("plugins");
+    private static AXDAScriptEngine plugin;
+    private static int[] version;
 
     @Override
     public void onLoad() {
+        plugin = this;
         System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
         getServer().getCommandMap().register("ase", new ASECommand());
         ScriptLoader.init();
@@ -24,5 +27,23 @@ public class AXDAScriptEngine extends PluginBase {
     @Override
     public void onDisable() {
 
+    }
+
+    public int[] getVersion() {
+        if (version != null) return version;
+        try {
+            String[] version = getDescription().getVersion().split("\\.");
+            int[] v = new int[3];
+            for (int i = 0; i < version.length; i++) {
+                v[i] = Integer.parseInt(version[i]);
+            }
+            return v;
+        } catch (Throwable t) {
+            return new int[]{0, 0, 0};
+        }
+    }
+
+    public static AXDAScriptEngine getPlugin() {
+        return plugin;
     }
 }
--- src/main/java/net/axda/se/api/script/LL.java
+++ src/main/java/net/axda/se/api/script/LL.java
@@ -1,11 +1,66 @@
 package net.axda.se.api.script;
 
+import cn.nukkit.Server;
+import net.axda.se.AXDAScriptEngine;
 import net.axda.se.api.API;
 import org.graalvm.polyglot.HostAccess;
 
 import java.util.Map;
 
 public class LL extends API {
+
+    @HostAccess.Export
+    public final String language;
+
+    @HostAccess.Export
+    public final int major;
+
+    @HostAccess.Export
+    public final int minor;
+
+    @HostAccess.Export
+    public final int revision;
+
+    @HostAccess.Export
+    public final int status = 0;
+
+    @HostAccess.Export
+    public final String scriptEngineVersion = "22.3.0";
+
+    @HostAccess.Export
+    public final boolean isWine = false;
+
+    @HostAccess.Export
+    public final boolean isDebugMode = false;
+
+
+    @HostAccess.Export
+    public final boolean isBeta;
+
+
+    @HostAccess.Export
+    public final boolean isDev;
+
+
+    @HostAccess.Export
+    public final boolean isRelease;
+
+
+    @HostAccess.Export
+    public final String pluginsRoot = AXDAScriptEngine.PLUGIN_PATH.getAbsolutePath();
+
+    public LL() {
+        Server server = Server.getInstance();
+        this.language = server.getLanguage().getLang();
+        AXDAScriptEngine plugin = AXDAScriptEngine.getPlugin();
+        int[] version = plugin.getVersion();
+        this.major = version[0];
+        this.minor = version[1];
+        this.revision = version[2];
+        this.isDev = status == 0;
+        this.isBeta = status == 1;
+        this.isRelease = status == 2;
+    }
 
     @HostAccess.Export
     public void registerPlugin(String name, String desc, Object[] ver, Map<String, Object> info) {

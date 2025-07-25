6548d4094b0603a66a10e8cd75259b5df50b0636
Ruokwok <i@ruok.cc>
2025-07-25 23:42:21 +0800

    整理插件命令

--- src/main/java/net/axda/se/ASECommand.java
+++ src/main/java/net/axda/se/ASECommand.java
@@ -2,42 +2,53 @@ package net.axda.se;
 
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.utils.TextFormat;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ASECommand extends Command {
 
+    private Map<String, ScriptCommand> cmds = new HashMap<>();
+
     public ASECommand() {
         super("ase");
+        cmds.put("reload", this::reload);
+        cmds.put("ls", this::ls);
     }
 
     @Override
     public boolean execute(CommandSender sender, String s, String[] args) {
         if (sender.isOp()) {
-            if (args[0].equals("reload")) {
-                reload();
-            } else if (args[0].equals("ls")) {
-                ls(sender);
+            if (cmds.containsKey(args[0])) {
+                return cmds.get(args[0]).execute(sender, args);
             }
         }
         return false;
     }
 
-    public void reload() {
+    public boolean reload(CommandSender sender, String[] args) {
         ScriptLoader.getInstance().disablePlugins();
         ScriptLoader.getInstance().loadPlugins(AXDAScriptEngine.PLUGIN_PATH);
+        return true;
     }
 
-    public void ls(CommandSender sender) {
+    public boolean ls(CommandSender sender, String[] args) {
         ScriptLoader loader = ScriptLoader.getInstance();
         Collection<ScriptEngine> engines = loader.getScriptMap().values();
         StringBuilder sb = new StringBuilder();
-        sb.append("Script Plugins (").append(engines.size()).append("):");
+        sb.append("Script Plugins (").append(engines.size()).append("):" + TextFormat.getByChar('a'));
         for (ScriptEngine engine : loader.getScriptMap().values()) {
             sb.append(" ").append(engine.getDescription().getName()).append(" v")
                     .append(engine.getDescription().getVersionStr())
-                    .append(",");
+                    .append(TextFormat.getByChar('r') + ",");
         }
         sender.sendMessage(sb.toString());
+        return true;
+    }
+
+    public interface ScriptCommand {
+        boolean execute(CommandSender sender, String[] args);
     }
 }

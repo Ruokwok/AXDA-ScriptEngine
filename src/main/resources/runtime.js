dc2115c1f6fa2af22da8326623fd65b978a78e70
Ruokwok <i@ruok.cc>
2025-08-27 23:56:18 +0800

    添加命令权限枚举

--- src/main/resources/runtime.js
+++ src/main/resources/runtime.js
@@ -1,8 +1,8 @@
 print = null;
 file = File;
 Object.freeze(ll);
-
-const Format = {
+Object.freeze(mc);
+const Format = Object.freeze({
     Black: "§0",
     DarkBlue: "§1",
     DarkGreen: "§2",
@@ -26,10 +26,30 @@ const Format = {
     StrikeThrough: "§m",
     Random: "§r",
     Clear: "§r",
-};
-Object.freeze(Format);
-const PermType = {
+});
+const PermType = Object.freeze({
     Any: 0,
     GameMasters: 1,
-    Console: 4
-};
+    Console: 2
+});
+const ParamType = Object.freeze({
+    Bool: 0,
+    Int: 1,
+    Float: 2,
+    String: 3,
+    Actor: 4,
+    Player: 5,
+    BlockPos: 6,
+    Vec3: 7,
+    RawText: 8,
+    Message: 9,
+    JsonValue: 10,
+    Item: 11,
+    Block: 12,
+    Effect: 13,
+    Enum: 14,
+    SoftEnum: 15,
+    ActorType: 16,
+    GameMode: 17,
+    Command: 18,
+});

53e733e81cd517363292eb2a69c327dfb1789596
Ruokwok <i@ruok.cc>
2025-08-29 15:41:56 +0800

    实现方向角对象

---
+++ src/main/java/net/axda/se/api/game/data/DirectionAngle.java
@@ -0,0 +1,13 @@
+package net.axda.se.api.game.data;
+
+import cn.nukkit.entity.Entity;
+
+public class DirectionAngle {
+
+    private Entity entity;
+
+    public DirectionAngle(Entity entity) {
+        this.entity = entity;
+    }
+
+}
